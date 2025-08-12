b1b862ce3cdd6f5a1412007761aaf9cc3c6e5e6f
AkuSenpai <akshaykaushik1234567@gmail.com>
2025-08-12 11:33:30 +0530

    Create python normal conversion code.py

---
+++ python normal conversion code.py
@@ -0,0 +1,21 @@
+meter=float(input("Enter the value of meter: "))
+kilometer=meter/1000
+print(meter,"meter to kilometer",kilometer)
+kilometer=float(input("Enter the value of kilometer: "))
+meter=kilometer*1000
+print(kilometer,"kilometer to meter",meter)
+meter=float(input("Enter the value of meter: "))
+cm=meter*100
+print(meter,"meter to cm",cm)
+cm=float(input("Enter the value of cm: "))
+meter=cm/100
+print(cm,"cm to meter",meter)
+kilometer=float(input("Enter the value of kilometer: "))
+cm=kilometer*100000
+print(kilometer,"kilometer to cm",cm)
+cm=float(input("Enter the value of cm: "))
+kilometer=cm/100000
+print(cm,"cm to kilometer",kilometer)
+cm=float(input("Enter the value of cm: "))
+mm=cm*10
+print(cm,"cm to mm",mm)
