eb201777b9f92272f4b93debc20c412764a86f53
AkuSenpai <akshaykaushik1234567@gmail.com>
2025-08-21 11:55:38 +0530

    Add files via upload

---
+++ binary,octal and hexa.py
@@ -0,0 +1,14 @@
+x1=10
+bin(x1)
+y1=bin(x1)
+print("This is a binary:",y1)
+
+x2=20
+oct(x2)
+y2=oct(x2)
+print("This is a octal: ",y2)
+
+x3=10
+hex(x3)
+y3=hex(x3)
+print("This is a hexa decimal: ",y3)
