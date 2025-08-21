3ee3ed6d55cfa6ef9b5d67b4310043c1f51f9527
AkuSenpai <akshaykaushik1234567@gmail.com>
2025-08-21 11:47:20 +0530

    Add files via upload

---
+++ Stirngs.py
@@ -0,0 +1,8 @@
+#using triple quotes
+print('''Oh no ", she exclaimed, " Ben's bike is broken!''')
+
+#Triple quoted strings can even span multiple lines.
+message='''This message will
+.....span several
+.....lines.'''
+print(message)
