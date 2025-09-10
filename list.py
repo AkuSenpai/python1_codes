e8ef892dd0e0c86f4b089c70053a2fe2d27ad822
AkuSenpai <akshaykaushik1234567@gmail.com>
2025-09-10 11:31:00 +0530

    list.py

--- list.py
+++ list.py
@@ -1,17 +1,55 @@
-# num=[1,2,3,4,5]
+
+# num=[1,2,3,4,5,6]
 # print("orginal list:",num)
-# num.append(7)
-# print("after append:",num)
-# num.insert(0,6)
-# print("after Insert:",num)
-# num.remove(2)
-# print("after remove:",num)
-# num.sort()
-# print("sort:",num)
-# num.reverse()
-# print("reverse:",num)
-# print("length of list:",len(num))
-# print("first element:",num[0])
-# print("last element:",num[-1])
-num=input("Enter items:").split()
-print("your list is:",num)
+
+# # num.append(7)
+# # print("after append:",num)
+
+# # num.insert(5,6)
+# # print("after Insert:",num)
+
+# # num.remove(2)
+# # print("after remove:",num)
+
+# # num.sort()
+# # print("sort:",num)
+
+# # num.reverse()
+# # print("reverse:",num)
+
+# # num.extend([8,9])
+# # print("Extend:",num)
+
+# # print(sum(num))
+
+# # print(num.count(6))
+
+# # len(num)
+# # print("length:",num)
+
+# # print(num.index(6))
+
+# # print(min(num))
+
+# # print(max(num))
+
+# # print(num.pop(2))
+# # print(num)
+
+# # del num[1]
+# # print("Delete:",num) #if you want to delete whole liste use "del num()
+
+# # num.remove(3)
+# # print("after remove:",num)
+
+# # num.sort(key=None, reverse=False)
+# # print("Sorted ascending:", num)
+
+# # num.sort(key=None, reverse=True)
+# # print("Sorted descending:", num)
+
+# # list.copy(num)
+# # print("Copy:",num)
+
+# # num.clear()
+# # print("clear:",num)
