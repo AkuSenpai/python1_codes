2b644bc50eea6f7f40fde9baad5c973a5b3b40ff
AkuSenpai <akshaykaushik1234567@gmail.com>
2025-09-10 11:37:37 +0530

    Update list.py

--- list.py
+++ list.py
@@ -17,59 +17,62 @@ print(l[-7:-2:1])
 print(l[-6:-1:1])
 print(l[:3])
 print(l[6:])
-# num=[1,2,3,4,5,6]
-# print("orginal list:",num)
+num=[1,2,3,4,5,6]
+print("orginal list:",num)
 
-# # num.append(7)
-# # print("after append:",num)
+num.append(7)
+print("after append:",num)
 
-# # num.insert(5,6)
-# # print("after Insert:",num)
+num.insert(5,6)
+print("after Insert:",num)
 
-# # num.remove(2)
-# # print("after remove:",num)
+num.remove(2)
+print("after remove:",num)
 
-# # num.sort()
-# # print("sort:",num)
+num.sort()
+print("sort:",num)
 
-# # num.reverse()
-# # print("reverse:",num)
+num.reverse()
+print("reverse:",num)
 
-# # num.extend([8,9])
-# # print("Extend:",num)
+num.extend([8,9])
+print("Extend:",num)
 
-# # print(sum(num))
+print(sum(num))
 
-# # print(num.count(6))
+print(num.count(6))
 
-# # len(num)
-# # print("length:",num)
+len(num)
+print("length:",num)
 
-# # print(num.index(6))
+print(num.index(6))
 
-# # print(min(num))
+print(min(num))
 
-# # print(max(num))
+print(max(num))
 
-# # print(num.pop(2))
-# # print(num)
+print(num.pop(2))
+print(num)
 
-# # del num[1]
-# # print("Delete:",num) #if you want to delete whole liste use "del num()
+del num[1]
+print("Delete:",num) #if you want to delete whole liste use "del num()
 
-# # num.remove(3)
-# # print("after remove:",num)
+num.remove(3)
+print("after remove:",num)
 
-# # num.sort(key=None, reverse=False)
-# # print("Sorted ascending:", num)
+num.sort(key=None, reverse=False)
+print("Sorted ascending:", num)
 
-# # num.sort(key=None, reverse=True)
-# # print("Sorted descending:", num)
+num.sort(key=None, reverse=True)
+print("Sorted descending:", num)
 
-# # list.copy(num)
+list.copy(num)
 
-# # print("Copy:",num)
+print("Copy:",num)
 
-# # num.clear()
-# # print("clear:",num)
+num.clear()
+print("clear:",num)
+Name="Akshaya kaushik"
+print(Name[::-1])
+
 
